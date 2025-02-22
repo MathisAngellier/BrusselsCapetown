635339d9f97817b8a8905adb35c9f2ead7809c99
MathisAngellier <109741444+MathisAngellier@users.noreply.github.com>
2025-02-22 15:57:27 +0100

    updated navbar

--- httpdocs/js/script.js
+++ httpdocs/js/script.js
@@ -6,12 +6,48 @@ document.addEventListener("DOMContentLoaded", function () {
     const dropbtn = dropdown.querySelector(".dropbtn");
     const dropdownContent = dropdown.querySelector(".dropdown-content");
     const links = dropdownContent.querySelectorAll("a");
-
     dropbtn.addEventListener("keydown", (e) => {
       if (e.key === "Enter" || e.key === " ") {
         e.preventDefault();
         dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
       }
     });
+  });
+
+  var swiper = new Swiper("#client-carousel", {
+    loop: true,
+    speed: 800,
+    slidesPerView: "auto",
+    preventInteractionOnTransition: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
   });
 });
