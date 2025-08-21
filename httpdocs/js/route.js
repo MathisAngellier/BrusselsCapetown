524732921f5ed808278ced5322f4e2bd60b8d266
MathisAngellier <109741444+MathisAngellier@users.noreply.github.com>
2025-08-21 15:53:22 +0200

    udat

---
+++ httpdocs/js/route.js
@@ -0,0 +1,18 @@
+const video = document.getElementById("responsive-video");
+
+function setVideoSource() {
+  const isMobile = window.innerWidth <= 768;
+  const src = isMobile ? "../video/Route-Mobile-16x9.mp4" : "../video/Opener Route.mp4";
+
+  if (video.src !== location.origin + "/" + src) {
+    video.src = src;
+    video.load();
+    video.play();
+  }
+}
+
+// Set on load
+window.addEventListener("DOMContentLoaded", setVideoSource);
+
+// Optional: update on resize
+window.addEventListener("resize", setVideoSource);
