aaf1ebb153f3a0b4565203cf0fdee36e5fba029e
MathisAngellier <109741444+MathisAngellier@users.noreply.github.com>
2025-08-28 00:50:02 +0200

    updated vite config and added gitignore for dist

--- httpdocs/vite.config.js
+++ httpdocs/vite.config.js
@@ -5,7 +5,18 @@ export default defineConfig({
   root: "src",
   publicDir: "../public", // If your static assets are in public/
   build: {
-    outDir: "../dist",
-    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        about: "src/views/about.html",
+        bicycle: "src/views/bicycle.html",
+        contact: "src/views/contact.html",
+        countries: "src/views/countries.html",
+        equipment: "src/views/equipment.html",
+        images: "src/views/images.html",
+        project: "src/views/project.html",
+        route: "src/views/route.html",
+      },
+    },
   },
 });
