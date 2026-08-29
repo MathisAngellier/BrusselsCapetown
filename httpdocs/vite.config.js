0437d4c438d6a61962758b591926d19443f26b79
MathisAngellier <109741444+MathisAngellier@users.noreply.github.com>
2026-08-30 00:24:44 +0200

    fixed navigation and made clean urls

--- httpdocs/vite.config.js
+++ httpdocs/vite.config.js
@@ -1,7 +1,35 @@
+import fs from "fs";
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+function cleanUrls() {
+  const routes = {
+    "/about": "/views/about.html",
+    "/bicycle": "/views/bicycle.html",
+    "/contact": "/views/contact.html",
+    "/countries": "/views/countries.html",
+    "/equipment": "/views/equipment.html",
+    "/gallery": "/views/gallery.html",
+    "/images": "/views/images.html",
+    "/project": "/views/project.html",
+    "/route": "/views/route.html",
+  };
+  return {
+    name: "clean-urls",
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        const pathname = req.url.split("?")[0];
+        if (routes[pathname]) {
+          req.url = routes[pathname];
+        }
+        next();
+      });
+    },
+  };
+}
+
 export default defineConfig({
+  plugins: [cleanUrls()],
   root: "src",
   publicDir: "../public",
   build: {
