a62310d5d3c8db9133886462fdc9f2910f510e73
MathisAngellier <109741444+MathisAngellier@users.noreply.github.com>
2026-09-01 19:25:59 +0200

    updated Brussels gallery

--- httpdocs/src/assets/js/galleryData.js
+++ httpdocs/src/assets/js/galleryData.js
@@ -22,6 +22,206 @@ export const galleryLocations = [
 
     media: [
       {
+        type: "image",
+        src: "/img/gallery/Brussels12.jpg",
+        alt: {
+          en: "Brussels photo 12",
+          fr: "Photo de Bruxelles 12",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels1.jpg",
+        alt: {
+          en: "Brussels photo 1",
+          fr: "Photo de Bruxelles 1",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels2.jpg",
+        alt: {
+          en: "Brussels photo 2",
+          fr: "Photo de Bruxelles 2",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels3.jpg",
+        alt: {
+          en: "Brussels photo 3",
+          fr: "Photo de Bruxelles 3",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels4.jpg",
+        alt: {
+          en: "Brussels photo 4",
+          fr: "Photo de Bruxelles 4",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels5.jpg",
+        alt: {
+          en: "Brussels photo 5",
+          fr: "Photo de Bruxelles 5",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels6.jpg",
+        alt: {
+          en: "Brussels photo 6",
+          fr: "Photo de Bruxelles 6",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels7.jpg",
+        alt: {
+          en: "Brussels photo 7",
+          fr: "Photo de Bruxelles 7",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels8.jpg",
+        alt: {
+          en: "Brussels photo 8",
+          fr: "Photo de Bruxelles 8",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels9.jpg",
+        alt: {
+          en: "Brussels photo 9",
+          fr: "Photo de Bruxelles 9",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels10.jpg",
+        alt: {
+          en: "Brussels photo 10",
+          fr: "Photo de Bruxelles 10",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels11.jpg",
+        alt: {
+          en: "Brussels photo 11",
+          fr: "Photo de Bruxelles 11",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels13.jpg",
+        alt: {
+          en: "Brussels photo 13",
+          fr: "Photo de Bruxelles 13",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels14.jpg",
+        alt: {
+          en: "Brussels photo 14",
+          fr: "Photo de Bruxelles 14",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels17.jpg",
+        alt: {
+          en: "Brussels photo 15",
+          fr: "Photo de Bruxelles 15",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels16.jpg",
+        alt: {
+          en: "Brussels photo 16",
+          fr: "Photo de Bruxelles 16",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels15.jpg",
+        alt: {
+          en: "Brussels photo 17",
+          fr: "Photo de Bruxelles 17",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels18.jpg",
+        alt: {
+          en: "Brussels photo 18",
+          fr: "Photo de Bruxelles 18",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels19.jpg",
+        alt: {
+          en: "Brussels photo 19",
+          fr: "Photo de Bruxelles 19",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels20.jpg",
+        alt: {
+          en: "Brussels photo 20",
+          fr: "Photo de Bruxelles 20",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels21.jpg",
+        alt: {
+          en: "Brussels photo 21",
+          fr: "Photo de Bruxelles 21",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels23.jpeg",
+        alt: {
+          en: "Brussels photo 23",
+          fr: "Photo de Bruxelles 23",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/Brussels22.jpeg",
+        alt: {
+          en: "Brussels photo 22",
+          fr: "Photo de Bruxelles 22",
+        },
+      },
+      {
+        type: "image",
+        src: "/img/gallery/departure.jpg",
+        alt: {
+          en: "Departure photo",
+          fr: "Photo de départ",
+        },
+      },
+      {
+        type: "video",
+        src: "/video/gallery/departure-video.mp4",
+        alt: {
+          en: "Departure video",
+          fr: "Vidéo de départ",
+        },
+      },
+      {
         type: "video",
         src: "/video/gallery/Masicorp.mp4",
         alt: {
@@ -29,174 +229,6 @@ export const galleryLocations = [
           fr: "Vidéo de Masicorp",
         },
       },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels12.jpg",
-      //     alt: {
-      //       en: "Brussels photo 12",
-      //       fr: "Photo de Bruxelles 12",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels1.jpg",
-      //     alt: {
-      //       en: "Brussels photo 1",
-      //       fr: "Photo de Bruxelles 1",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels2.jpg",
-      //     alt: {
-      //       en: "Brussels photo 2",
-      //       fr: "Photo de Bruxelles 2",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels3.jpg",
-      //     alt: {
-      //       en: "Brussels photo 3",
-      //       fr: "Photo de Bruxelles 3",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels4.jpg",
-      //     alt: {
-      //       en: "Brussels photo 4",
-      //       fr: "Photo de Bruxelles 4",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels5.jpg",
-      //     alt: {
-      //       en: "Brussels photo 5",
-      //       fr: "Photo de Bruxelles 5",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels6.jpg",
-      //     alt: {
-      //       en: "Brussels photo 6",
-      //       fr: "Photo de Bruxelles 6",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels7.jpg",
-      //     alt: {
-      //       en: "Brussels photo 7",
-      //       fr: "Photo de Bruxelles 7",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels8.jpg",
-      //     alt: {
-      //       en: "Brussels photo 8",
-      //       fr: "Photo de Bruxelles 8",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels9.jpg",
-      //     alt: {
-      //       en: "Brussels photo 9",
-      //       fr: "Photo de Bruxelles 9",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels10.jpg",
-      //     alt: {
-      //       en: "Brussels photo 10",
-      //       fr: "Photo de Bruxelles 10",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels11.jpg",
-      //     alt: {
-      //       en: "Brussels photo 11",
-      //       fr: "Photo de Bruxelles 11",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels13.jpg",
-      //     alt: {
-      //       en: "Brussels photo 13",
-      //       fr: "Photo de Bruxelles 13",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels14.jpg",
-      //     alt: {
-      //       en: "Brussels photo 14",
-      //       fr: "Photo de Bruxelles 14",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels17.jpg",
-      //     alt: {
-      //       en: "Brussels photo 15",
-      //       fr: "Photo de Bruxelles 15",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels16.jpg",
-      //     alt: {
-      //       en: "Brussels photo 16",
-      //       fr: "Photo de Bruxelles 16",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels15.jpg",
-      //     alt: {
-      //       en: "Brussels photo 17",
-      //       fr: "Photo de Bruxelles 17",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels18.jpg",
-      //     alt: {
-      //       en: "Brussels photo 18",
-      //       fr: "Photo de Bruxelles 18",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels19.jpg",
-      //     alt: {
-      //       en: "Brussels photo 19",
-      //       fr: "Photo de Bruxelles 19",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels20.jpg",
-      //     alt: {
-      //       en: "Brussels photo 20",
-      //       fr: "Photo de Bruxelles 20",
-      //     },
-      //   },
-      //   {
-      //     type: "image",
-      //     src: "/img/gallery/Brussels21.jpg",
-      //     alt: {
-      //       en: "Brussels photo 21",
-      //       fr: "Photo de Bruxelles 21",
-      //     },
-      //   },
     ],
   },
 ];
